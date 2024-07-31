4d79e145a8b926e5f71761ff1fdee1502f392726
Rubens Albukrk <rubensf4rda@gmail.com>
2024-07-31 18:43:59 -0300

    test for build

--- app.config.js
+++ app.config.js
@@ -1,4 +1,12 @@
 export default {
+
+  "expo": {
+    "extra": {
+      "eas": {
+        "projectId": "9be865d4-5b79-48a0-9df2-7982d8efaf7a"
+      }
+    }
+  },
     android: {
       googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
       
